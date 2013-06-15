ff7312c5b9c3752eedda9be63ddb7283f2c439d8
Jan Lehnardt <jan@apache.org>
2013-06-15 22:27:02 +0200

    add support for Apache CouchDB.app

---
+++ bin/launch-couchdb-from-app.sh
@@ -0,0 +1,5 @@
+#/bin/sh -ex
+# thx @surryhill https://twitter.com/surryhill/status/345283578835390464
+BASE=`mdfind "kMDItemCFBundleIdentifier == org.apache.couchdb" | head -1`
+cd "$BASE"/Contents/Resources/couchdbx-core 
+./bin/couchdb $@
